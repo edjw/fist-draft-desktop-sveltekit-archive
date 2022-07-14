bdfeaf3d5a87aea49c9b0c1ce1e6e96c6cc38532
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2022-07-14 22:02:13 +0100

    initial. It works and some style updates

---
+++ src/components/stores.js
@@ -0,0 +1,10 @@
+import { writable } from "svelte-local-storage-store";
+
+
+
+export const contents = writable("preferences", {
+    datetime: "",
+    html: "",
+    contents: "",
+    plainText: ""
+})
