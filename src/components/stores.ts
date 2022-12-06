f273db3123291b089cae9dfcf674fc156fab2ff2
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2022-12-06 10:18:05 +0000

    format

--- src/components/stores.ts
+++ src/components/stores.ts
@@ -1,18 +1,18 @@
 import { writable as writableLocalStorage } from "svelte-local-storage-store";
-import { writable } from 'svelte/store';
+import { writable } from "svelte/store";
 
 const contents = writableLocalStorage("preferences", {
-    datetime: "",
-    html: "",
-    contents: {
-        "ops": []
-    },
-    plainText: ""
+  datetime: "",
+  html: "",
+  contents: {
+    ops: [],
+  },
+  plainText: "",
 });
 
 const copyButtonText = writable({
-    wordButtonText: "Copy",
-    markdownButtonText: "Copy as Markdown",
+  wordButtonText: "Copy",
+  markdownButtonText: "Copy as Markdown",
 });
 
 export { contents, copyButtonText };
